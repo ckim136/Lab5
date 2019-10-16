6ae5f205a1004835fab9d583b1817c883a796c73
CHAEEUN KIM <ckim136@my.bcit.ca>
2019-10-16 16:21:27 -0700

    added operation.hpp

---
+++ operation.hpp
@@ -0,0 +1,14 @@
+//
+// Created by kimem on 2019-10-16.
+//
+
+#ifndef LAB5_OPERATION_HPP
+#define LAB5_OPERATION_HPP
+using namespace std;
+class operation{
+public:
+    virtual char get_code() = 0;
+    virtual int perform(int, int) = 0;
+    virtual ~operation(){};
+};
+#endif //LAB5_OPERATION_HPP
