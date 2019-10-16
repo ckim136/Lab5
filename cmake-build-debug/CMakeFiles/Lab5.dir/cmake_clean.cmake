52c8a36feafa43bc7f9edb8ebc83dc9486c29b4e
CHAEEUN KIM <ckim136@my.bcit.ca>
2019-10-16 16:12:17 -0700

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/Lab5.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lab5.dir/main.cpp.o"
+  "Lab5.pdb"
+  "Lab5.exe"
+  "libLab5.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Lab5.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
